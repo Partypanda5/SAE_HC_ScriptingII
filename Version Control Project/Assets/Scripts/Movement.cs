56a8971283dd0210e55c9e4145c4854e6cfcbb35
Andrea Hayes <scoondi@hotmail.com>
2022-10-03 14:19:07 +0200

    Player Movement Progress
    I have made some changes to the player movement, it feels a lot better.

--- Version Control Project/Assets/Scripts/Movement.cs
+++ Version Control Project/Assets/Scripts/Movement.cs
@@ -7,7 +7,9 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //look at all this cool code wow look at me go
+        // this is insane
+        //cool cool cool
     }
 
     // Update is called once per frame
